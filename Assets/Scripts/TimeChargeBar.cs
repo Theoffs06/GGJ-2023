26019dc06dbffc31e56115b92f987c0bbafdd2bb
Nicolas Cadre <nicolas.cadre46@gmail.com>
2023-02-04 20:23:34 +0100

    More cam adjustement + Buf fixing with time charge

--- Assets/Scripts/TimeChargeBar.cs
+++ Assets/Scripts/TimeChargeBar.cs
@@ -16,41 +16,38 @@ public class TimeChargeBar : MonoBehaviour
     {
         switch(timeManager.TimeCharge)
         {
-            case int n when (n == 0):
+            case int n when (n >= 0 & n < 10):
                 image.sprite = sprites[0];
                 break;
-            case int n when (n > 0 & n < 10):
+            case int n when (n >= 10 & n < 20):
                 image.sprite = sprites[1];
                 break;
-            case int n when (n >= 10 & n < 20):
+            case int n when (n >= 20 & n < 30):
                 image.sprite = sprites[2];
                 break;
-            case int n when (n >= 20 & n < 30):
+            case int n when (n >= 30 & n < 40):
                 image.sprite = sprites[3];
                 break;
-            case int n when (n >= 30 & n < 40):
+            case int n when (n >= 40 & n < 50):
                 image.sprite = sprites[4];
                 break;
-            case int n when (n >= 40 & n < 50):
+            case int n when (n >= 50 & n < 60):
                 image.sprite = sprites[5];
                 break;
-            case int n when (n >= 50 & n < 60):
+            case int n when (n >= 60 & n < 70):
                 image.sprite = sprites[6];
                 break;
-            case int n when (n >= 60 & n < 70):
+            case int n when (n >= 70 & n < 80):
                 image.sprite = sprites[7];
                 break;
-            case int n when (n >= 70 & n < 80):
+            case int n when (n >= 80 & n < 90):
                 image.sprite = sprites[8];
                 break;
-            case int n when (n >= 80 & n < 90):
+            case int n when (n >= 90 & n < 100):
                 image.sprite = sprites[9];
                 break;
-            case int n when (n >= 90 & n < 100):
-                image.sprite = sprites[10];
-                break;
             case int n when (n == 100):
-                image.sprite = sprites[11];
+                image.sprite = sprites[10];
                 break;
 
 
