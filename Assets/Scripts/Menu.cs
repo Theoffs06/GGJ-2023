abba9d9cf64d390c79c413d816695de6b08a42ef
Nicolas Cadre <nicolas.cadre46@gmail.com>
2023-02-05 17:18:45 +0100

    no message

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -16,13 +16,11 @@ public class Menu : MonoBehaviour
     {
         gameObject.SetActive(false);
         Paused = true;
-        Time.timeScale = 1;
     }
     public void Pause()
     {
         gameObject.SetActive(true);
         Paused = false;
-        Time.timeScale = 0;
     }
 
     public void Exit()
